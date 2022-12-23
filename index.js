0e4ea002b7fc0d019504ea85abd7862923abdd8b
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-12-23 11:40:14 -0300

    make sure that mongoose.connect is finished before allowing your app to serve requests

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ const CONNECTION_URL = process.env.MONGODB_URI || "mongodb://localhost:27017/for
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(CONNECTION_URL);
+
+		console.info(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
 if (!isDev) {
 	app.use(limiter);
 }
@@ -42,13 +53,12 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-mongoose
-	.connect(CONNECTION_URL)
-	.then(() =>
+connectDB()
+	.then(() => {
 		app.listen(PORT, () => {
 			console.info(`Forito listening on port ${PORT}`);
-		})
-	)
+		});
+	})
 	.catch(err => {
 		console.error(err);
 	});
